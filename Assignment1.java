1147257917540da858396196e2faaa0651d09f1b
pkuselias <pkuselias@hartford.edu>
2025-02-05 15:20:22 -0500

    Ask for number in Fahrenheit

--- Assignment1.java
+++ Assignment1.java
@@ -3,6 +3,7 @@
 public class Assignment1 {
   public static void main(String[] args) {
     // Add your code here
+
     System.out.println("PPPPPPPP   KKK     KKK");
     System.out.println("PPP    PPP KKK    KKK ");
     System.out.println("PPP    PPP KKK   KKK  ");
@@ -10,5 +11,9 @@ public class Assignment1 {
     System.out.println("PPP        KKK   KKK  ");
     System.out.println("PPP        KKK    KKK ");
     System.out.println("PPP        KKK     KKK");
+
+    System.out.println("");
+
+    System.out.print("Please enter a number in Fahrenheit: ");
   }
 }
