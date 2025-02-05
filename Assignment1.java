2caf87146aec546afe8b0eac373b406fa8636e44
pkuselias <pkuselias@hartford.edu>
2025-02-05 18:27:50 -0500

    Generate random number (32 <= x <= 16384)

--- Assignment1.java
+++ Assignment1.java
@@ -37,5 +37,11 @@ public class Assignment1 {
     String modified;
     modified = "" + third + second + first;     //Reverse order of the three remaining characters
 
+    Random generator = new Random();
+    int number;
+    number = generator.nextInt(16353) + 32;     //Generate randomly from 0 to 16352 (inclusive), then add 32 (inclusive 32 to 16384)
+
+
+
   }
 }
