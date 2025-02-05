7e17ace105e02fb8a7080218ecb49decd0373025
pkuselias <pkuselias@hartford.edu>
2025-02-05 15:06:49 -0500

    Print initials in block letters

--- Assignment1.java
+++ Assignment1.java
@@ -3,5 +3,12 @@
 public class Assignment1 {
   public static void main(String[] args) {
     // Add your code here
+    System.out.println("PPPPPPPP   KKK     KKK");
+    System.out.println("PPP    PPP KKK    KKK ");
+    System.out.println("PPP    PPP KKK   KKK  ");
+    System.out.println("PPPPPPPP   KKKKKK     ");
+    System.out.println("PPP        KKK   KKK  ");
+    System.out.println("PPP        KKK    KKK ");
+    System.out.println("PPP        KKK     KKK");
   }
 }
